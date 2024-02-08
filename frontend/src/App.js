5bf593777802317e577948ffa59593386eb25e62
Prescilla Quiban <quibapre@gmail.com>
2024-02-08 17:34:08 +0800

    Implement React Router Product component & homescreen Header & footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to online ecommerce</h1>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
